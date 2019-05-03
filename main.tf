57b9aa00646cc84c21ae8f01fc46fcaa169c4f2b
robert <robert@recipesavants.com>
2019-05-03 11:00:58 -0500

    instanceCount

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "efk" {
     source = "modules/efk"
+    instanceCount = 2
 }
 
 module "ms"{
     source = "modules/ms"
+    instanceCount = 3
 }
---
+++ variables.tf
@@ -0,0 +1,24 @@
+resource "random_pet" "environment" {
+}
+
+variable "environment" {
+  type        = "string"
+  description = "Name of environment. Used to tag resources."
+  default     = ""
+}
+
+variable "subnet" {
+  type        = "string"
+  description = "Subnet of environment. Used to partition out the address space. Use a /16 space."
+  default     = "15.0.0.0/16"
+}
+
+variable "instanceCount" {
+  type = string
+  desciption = "desc"
+  default = "2"
+}
+
+locals {
+  environment = "${var.environment == "" ? random_pet.environment.id : var.environment}"
+}
